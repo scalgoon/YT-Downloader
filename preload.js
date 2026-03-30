aed90d9b1e4aa70ff88644f21e38e3f204d9bbd9
Dez P <scalgoon@gmail.com>
2026-03-30 11:09:13 -0500

    Update preload.js

---
+++ preload.js
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer } = require("electron");
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+contextBridge.exposeInMainWorld('myAPI', {
+    sendNotif: (title, body) => ipcRenderer.send('system:sendNotification', { title, body }),
+    startProgress: () => ipcRenderer.invoke('system:startProgress'),
+    stopProgress: () => ipcRenderer.invoke('system:stopProgress'),
+});
